fc1ba4e69a433b062da8c510ca94d51879c0a75f
Bernhard Rode <ebborode@amazon.com>
2025-09-18 22:48:05 +0200

    Fix build packages and remove backup file creation

--- build-snap.sh
+++ build-snap.sh
@@ -5,7 +5,7 @@ echo "Building AWS Greengrass Lite snap package..."
 
 # Update version in snapcraft.yaml from version file
 VERSION=$(cat version)
-sed -i.bak "s/version: '.*'/version: '$VERSION'/" snap/snapcraft.yaml
+sed -i "s/version: '.*'/version: '$VERSION'/" snap/snapcraft.yaml
 
 # Build the snap using snapcraft pack
 snapcraft pack --destructive-mode
